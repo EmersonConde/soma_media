9481d2de36106ce005369f6ddfd62010c6b55616
Émerson Conde <emersonsilvacaucaia27@gmail.com>
2024-11-03 10:55:56 +0000

    Exe add

--- obj/Debug/net8.0/soma_media.AssemblyInfo.cs
+++ obj/Debug/net8.0/soma_media.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("soma_media")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cfa169c9aedc85703caff4505742371936f18f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("soma_media")]
 [assembly: System.Reflection.AssemblyTitleAttribute("soma_media")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
